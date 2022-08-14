ae45f822f76e7c9157e62b14aa4ad5f977fb1051
Lizwe MacDonald Ginyani <68953760+lizwe-mac@users.noreply.github.com>
2022-08-14 16:28:31 +0200

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,12 +1,12 @@
 const firebase = require("firebase");
 const firebaseConfig = {
-  apiKey: "AIzaSyAy3-mHqfNyj7uHr4AVD--DPd7nw4_UNHQ",
-  authDomain: "lizwejs.firebaseapp.com",
-  projectId: "lizwejs",
-  storageBucket: "lizwejs.appspot.com",
-  messagingSenderId: "1042933831937",
-  appId: "1:1042933831937:web:2872e192ba4b5ba5796300",
-  measurementId: "G-2GLG1PW5ZJ",
+  apiKey: 'Your API Key' ,
+  authDomain: "[redacted]",
+  projectId: "[redacted]",
+  storageBucket: "[redacted]",
+  messagingSenderId: "[redacted]",
+  appId: "[redacted]",
+  measurementId: "[redacted]",
 };
 
 // Initialize Firebase
